b24d10e828a46d3c7f0d8cb1bc2cd87b8c13395c
CharlesLuis@Dev <charles.luis1724@gmail.com>
2026-06-07 01:26:47 +0100

    Add files via upload

--- Parallax_effect.js
+++ Parallax_effect.js
@@ -3,7 +3,7 @@ gsap.registerPlugin(ScrollTrigger);
 const {innerHeight} = window;
 
 gsap.from('.zoom_out h1', {
-    scale: 10, stager: 0.25, duration: 3, scrollTrigger: {
+    scale: 10, stager: 0.25, duration: 10, scrollTrigger: {
         trigger: '.zoom_out',
         pin: true,
         end: `+=${innerHeight * 1.3}`,
@@ -12,11 +12,11 @@ gsap.from('.zoom_out h1', {
 });
 
 gsap.to('.zoom_in h1', {
-    scale: 100, stager: 0.25, duration: 3, scrollTrigger: {
+    scale: 50, duration: 10, scrollTrigger: {
         trigger: '.zoom_in',
         pin: true,
         end: `+=${innerHeight * 1.3}`,
-        scrub: 3
+        scrub: 10
     }
 });
 
